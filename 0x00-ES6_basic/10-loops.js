66405e95b9fe49dc4100727124ffc2383cd2aee1
Vinex7 <thabelosilima@gmail.com>
2024-08-04 14:29:14 -0700

    C update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // get both the index and word with entries
-  for (let [idx, word] of array.entries()) {
-    array[idx] = `${appendString}${word}`;
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
   }
 
-  return array;
+  return arr;
 }
